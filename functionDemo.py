f62e41341fe924423ee7b7975dcde61f14cd7850
christodragnev <35492777+christodragnev@users.noreply.github.com>
2018-03-09 08:28:23 -0500

    Updated from Brython Server: 3/9/2018 8:28:22 AM

--- functionDemo.py
+++ functionDemo.py
@@ -3,4 +3,31 @@
 #functionDemo.py - how to write your own functions
 
 def hw():
-    print('Hellow World")
+    print('Hello, world')
+    
+    
+def double(thingToDouble):
+    print(thingToDouble * 2)
+    
+
+def bigger(a,b):
+    if a>b:
+        print(a)
+    else:
+        print(b)
+        
+def slope(x1, y1, x2, y2):
+    print((y2-y1)/(x2-x1))
+
+slope(1,1,2,2)
+        
+#bigger(3,4)
+#bigger(4,4)
+#bigger('Smedinghoff','Sam')
+#bigger(True,False)
+    
+#double(12) #test of the double function
+#double('w') #test of double with a string input
+#double(False) #test of double with a boolean
+#hw() #test of our function
+#hw() #another
