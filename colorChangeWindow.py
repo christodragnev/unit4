e6f71bf0596167dfc99d966277b38aa5e88b2c2d
christodragnev <35492777+christodragnev@users.noreply.github.com>
2018-03-09 08:48:38 -0500

    Updated from Brython Server: 3/9/2018 8:48:37 AM

--- colorChangeWindow.py
+++ colorChangeWindow.py
@@ -1,1 +1,17 @@
+#Christo Dragnev
+#3/9/18
+#colorChangeWindow.py
 
+from ggame import *
+from  random import randint
+
+1 = Color(0xFF0000,1) #this is the color red
+2 = Color(0x00FF00,1)
+3 = Color(0x0000FF,1)
+
+def listenMouseEvent(a):
+    print(a)
+listenMouseEvent(randint(0,3))
+
+App().listenMouseEvent('click',mouseClick)
+App.run()
