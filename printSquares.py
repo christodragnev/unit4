947b21ed634009176fe1bd825892ba9d4af54d2c
christodragnev <35492777+christodragnev@users.noreply.github.com>
2018-03-26 08:41:25 -0400

    Updated from Brython Server: 3/26/2018 8:41:25 AM

--- printSquares.py
+++ printSquares.py
@@ -1,1 +1,13 @@
+#Christo Dragnev
+#3/26/18
+#printSquares.py
+
+def printSquares(h,b):
+    for i in range (h,b):
+        print('+','- - + '*b)
+        print('|     '*(b+1))
+        i+=1
+    print('+','- - + '*b)
+
+printSquares(2,4)
 
