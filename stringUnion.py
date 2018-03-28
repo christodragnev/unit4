cd99a7f17bbe2ad60b01b4ab230218feff567af4
christodragnev <35492777+christodragnev@users.noreply.github.com>
2018-03-28 08:29:26 -0400

    Updated from Brython Server: 3/28/2018 8:29:25 AM

--- stringUnion.py
+++ stringUnion.py
@@ -1,1 +1,11 @@
+#Christo Dragnev
+#3/28/18
+#stringUnion.py
 
+def stringUnion(a,b):
+    answer = ''
+    for ch in a and ch in b:
+        if ch != answer:
+            answer+=str(ch)
+            
+stringUnion('Mississippi','Pennsylvania')
