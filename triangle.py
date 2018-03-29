1c9ee2534a0d69e455394516971543a6dd8897a8
christodragnev <35492777+christodragnev@users.noreply.github.com>
2018-03-29 08:49:33 -0400

    Updated from Brython Server: 3/29/2018 8:49:33 AM

--- triangle.py
+++ triangle.py
@@ -1,1 +1,20 @@
+#Christo Dragnev
+#3/29/18
+#triangle.py
 
+def Area(x1,y1,x2,y2,x3,y3)
+def distance(x1,y1,x2,y2):
+    dist = (((x2-x1)**2)+((y2-y1)**2))**.5
+    return dist
+    
+print(distance(3,4,-5,2))
+def distance(x1,y1,x2,y2):
+    dist = (((x2-x1)**2)+((y2-y1)**2))**.5
+    return dist
+    
+print(distance(3,4,-5,2))
+def distance(x1,y1,x2,y2):
+    dist = (((x2-x1)**2)+((y2-y1)**2))**.5
+    return dist
+    
+print(distance(3,4,-5,2))
