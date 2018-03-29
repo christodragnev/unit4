0f3bfd1ef062368849789a23570306a9984cee11
christodragnev <35492777+christodragnev@users.noreply.github.com>
2018-03-29 09:37:17 -0400

    Updated from Brython Server: 3/29/2018 9:37:17 AM

--- multiply.py
+++ multiply.py
@@ -1,1 +1,20 @@
+#Christo Dragnev
+#3/29/18
+#multiply.py
 
+from random import randint
+
+def multiply(a,b):
+    a = randint(1,12)
+    b = randint(1,12)
+    while i<5:
+        answer = int(input('What is ',a, '*', b, '?')
+        if answer == a*b:
+            i+=1
+            answer = int(input('What is ',a, '*', b, '?')
+    
+        else:
+            answer = int(input('What is ',a, '*', b, '?')
+    
+        
+    
