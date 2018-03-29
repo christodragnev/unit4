ea79b10abb997099cad33bd6d1d35e202766e1d5
christodragnev <35492777+christodragnev@users.noreply.github.com>
2018-03-29 08:35:51 -0400

    Updated from Brython Server: 3/29/2018 8:35:50 AM

--- warmup12.py
+++ warmup12.py
@@ -1,1 +1,13 @@
+#Christo Dragnev
+#3/29/18
+#warmup12.py - function that returns the GCF of two numbers
+
+def gcf(a,b):
+    i=b
+    while i>0:
+        if a%i==0 and b%i==0:
+            return i
+        i-=1
+print(gcf(12,15))
+
 
