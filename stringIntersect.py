aeab237345df964e82ebe05612defa95928f474b
christodragnev <35492777+christodragnev@users.noreply.github.com>
2018-03-29 08:47:23 -0400

    Updated from Brython Server: 3/29/2018 8:47:23 AM

--- stringIntersect.py
+++ stringIntersect.py
@@ -1,1 +1,12 @@
+#Christo Dragnev
+#3/29/18
+#stringIntersect.py
 
+def stringIntersect(a,b):
+    answer = ''
+    for ch in a.lower():
+        if ch in b.lower() and ch not in answer:
+            answer+=ch
+        
+    return answer
+print(stringIntersect('Mississippi','Pennsylvania'))
