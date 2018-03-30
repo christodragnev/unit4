17841fe19dadbd939e381014147069f2a352fe51
christodragnev <35492777+christodragnev@users.noreply.github.com>
2018-03-30 08:50:41 -0400

    Updated from Brython Server: 3/30/2018 8:50:41 AM

--- multiply.py
+++ multiply.py
@@ -4,17 +4,29 @@
 
 from random import randint
 
-def multiply(a,b):
+def encouragingMessage():
+    randint(1,3)
+    if randint(1,3)==1:
+        print('Watch out world, here comes the next great multiplier')
+    elif randint(1,3)==2:
+        print('Next Einstein?')
+    else:
+        print('You are incredible!')
+    
+    
+i=0
+while i<5:
     a = randint(1,12)
     b = randint(1,12)
-    while i<5:
-        answer = int(input('What is ',a, '*', b, '?')
-        if answer == a*b:
-            i+=1
-            answer = int(input('What is ',a, '*', b, '?')
-    
-        else:
-            answer = int(input('What is ',a, '*', b, '?')
+    answer = int(input('What is '+str(a)+ '*'+str(b)+ '?'))
+    if answer == a*b:
+        i+=1
+    else:
+        print('Sorry, incorrect')
+    if i==5:
+        encouragingMessage()
+
+            
     
         
     
