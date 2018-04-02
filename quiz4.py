2aa2a0437fb626e141aa1c442ff4def0b8366e80
christodragnev <35492777+christodragnev@users.noreply.github.com>
2018-04-02 08:36:09 -0400

    Updated from Brython Server: 4/2/2018 8:36:08 AM

--- quiz4.py
+++ quiz4.py
@@ -1,1 +1,29 @@
+#Christo Dragnev
+#4/2/18
+#quiz4.py
 
+def count(a):
+    i = 1
+    while i<a:
+        print(i)
+        i+=1
+count(10)
+
+def excitedPrint(a):
+    print(a.upper()+'!!!')
+excitedPrint('I <3 Programming')
+
+def firstLetter(a):
+    for ch in a:
+        if ch in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
+            print(ch.upper())
+firstLetter("Smedinghoff")
+
+def repeats(a,b,c):
+    if a==b or a==c or b==c:
+        return True
+    else:
+        return False
+
+print(repeats(5,6,5))
+    
